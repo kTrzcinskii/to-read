0523d316ffb24d5ff37a3b9a5e49e9005386d3d7
kTrzcinskii <kt.trzcinski@gmail.com>
2022-08-02 13:40:34 +0200

    Book Container Navigation

---
+++ src/components/explore-page/BookNavigationBtns.tsx
@@ -0,0 +1,50 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import { Button, HStack, IconButton } from "@chakra-ui/react";
+import { Dispatch, SetStateAction } from "react";
+
+interface BookNavigationBtnsProps {
+  page: number;
+  setPage: Dispatch<SetStateAction<number>>;
+  numOfItems: number;
+  handleSearchClick: () => void;
+}
+
+const BookNavigationBtns: React.FC<BookNavigationBtnsProps> = ({
+  page,
+  setPage,
+  handleSearchClick,
+  numOfItems,
+}) => {
+  return (
+    <HStack py={4} px={4} bgColor='white' rounded='lg'>
+      <IconButton
+        aria-label='go to previous page'
+        disabled={page === 0}
+        icon={<ChevronLeftIcon fontSize='2xl' />}
+        onClick={() => {
+          setPage((prev) => prev - 1);
+          handleSearchClick();
+        }}
+        bgColor='white'
+        color='blue.500'
+        _hover={{ bgColor: "blue.500", color: "white" }}
+        _active={{}}
+      />
+      <IconButton
+        aria-label='go to next page'
+        disabled={numOfItems < 16}
+        icon={<ChevronRightIcon fontSize='2xl' />}
+        onClick={() => {
+          setPage((prev) => prev + 1);
+          handleSearchClick();
+        }}
+        bgColor='white'
+        color='blue.500'
+        _hover={{ bgColor: "blue.500", color: "white" }}
+        _active={{}}
+      />
+    </HStack>
+  );
+};
+
+export default BookNavigationBtns;
