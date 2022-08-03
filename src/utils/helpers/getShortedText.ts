611b4f75bdeb942346b27fbb447e98b8544c7068
kTrzcinskii <kt.trzcinski@gmail.com>
2022-08-03 18:41:53 +0200

    change name + added trim

--- src/utils/helpers/getShortedText.ts
+++ src/utils/helpers/getShortedText.ts
@@ -1,7 +1,7 @@
-export default function getCardTitle(title: string, maxLength: number) {
+export default function getShortedText(title: string, maxLength: number) {
   if (title.length <= maxLength) {
     return title;
   }
 
-  return title.slice(0, maxLength - 3) + "...";
+  return title.slice(0, maxLength - 3).trim() + "...";
 }
