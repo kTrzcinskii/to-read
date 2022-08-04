dae369f270ce446d99ee2e6e603c486b9b156206
kTrzcinskii <kt.trzcinski@gmail.com>
2022-08-04 22:18:45 +0200

    added new tags

--- src/utils/helpers/transformDescription.ts
+++ src/utils/helpers/transformDescription.ts
@@ -4,6 +4,12 @@ export default function transformDescription(text: string) {
   text = text.replaceAll("</b>", "");
   text = text.replaceAll("<i>", "");
   text = text.replaceAll("</i>", "");
+  text = text.replaceAll("<p>", "");
+  text = text.replaceAll("</p>", "");
+  text = text.replaceAll("<li>", "");
+  text = text.replaceAll("</li>", "");
+  text = text.replaceAll("<ul>", "");
+  text = text.replaceAll("</ul>", "");
 
   return text;
 }
